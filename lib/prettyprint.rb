cb79410bb6173928f21aa1ef2707253bec191ffd
Dan Corrigan <df.corrigan@gmail.com>
2013-12-21 18:43:23 -0500

    strips whitespace nodes effectively

--- lib/prettyprint.rb
+++ lib/prettyprint.rb
@@ -2,5 +2,36 @@ require 'nokogiri'
 
 class PrettyPrint
 
+  def initialize doc, options
+    @doc = doc
+    options.each do |name, value|
+      instance_variable_set("@#{name}", value)
+    end
+  end
+
+  def pp
+    strip_whitespace
+    @doc.serialize(:save_with => 0)
+  end
+
+  def strip_whitespace
+    ws_accessor = get_ws_accessor
+    @doc.css(ws_accessor).each do |node|
+      eliminate_ws_nodes_from node
+    end
+  end
+
+  def get_ws_accessor
+    ws_accessor = @preserve_whitespace ? @block : @block + @compact + @inline
+    ws_accessor.join(',')
+  end
+
+  def eliminate_ws_nodes_from node
+    node.children.each do |child|
+      next unless child.text?
+      child.remove if child.text.match /^\s*$/
+    end
+  end
+
 
 end
--- test/test_prettyprint.rb
+++ test/test_prettyprint.rb
@@ -8,19 +8,44 @@ class PrettyPrintTests < Minitest::Test
   OP1 = {
     :block => %w(root),
     :compact => %w(p),
-    :inline => %w(),
+    :inline => %w(i),
+    :preserve_whitespace => true,
+    :tab => '  '
+  }
+
+  OP2 = {
+    :block => %w(root),
+    :compact => %w(p),
+    :inline => %w(i),
     :preserve_whitespace => false,
     :tab => '  '
   }
 
-  def setup_and_exercise
+  def setup_and_exercise options
     @parsed = Nokogiri.XML @input
-    @pp = PrettyPrint.new(@parsed).pp
+    @pp = PrettyPrint.new(@parsed, options).pp
   end
 
-  def test_basic_stuff
-    @input = "<root><p>stuff</p></root>"
-    assert @input == 'foo'
+  def test_strips_basic_whitespace_from_block_when_ws_is_true
+    @input = "<root>  <p>stuff</p>  </root>"
+    setup_and_exercise OP1
+    assert @pp !~ /  /
+  end
+
+  def test_leaves_inline_and_compact_space_when_ws_is_true
+    @input = "<root>  <p> </p><p>stuff<i> </i></p>  </root>"
+    setup_and_exercise OP1
+    assert @pp =~ /<p> <\/p>/
+    assert @pp =~ /<i> <\/i>/
+    assert @pp !~ /<root>  <p>/
+  end
+
+  def test_strips_inline_and_compact_space_when_ws_is_false
+    @input = "<root>  <p> </p><p>stuff<i> </i></p>  </root>"
+    setup_and_exercise OP2
+    assert @pp =~ /<p\/>/
+    assert @pp =~ /<i\/>/
+    assert @pp !~ /<root>  <p>/
   end
 
 end
