7e1dd648000776e4a1629573c5f672bca455fda4
Dan Corrigan <df.corrigan@gmail.com>
2013-12-21 19:38:25 -0500

    basic prettyprint function seems to work

--- lib/prettyprint.rb
+++ lib/prettyprint.rb
@@ -1,9 +1,15 @@
 require 'nokogiri'
+require 'andand'
 
 class PrettyPrint
 
   def initialize doc, options
     @doc = doc
+    @preserve_whitespace = true
+    set_options_as_ivars options
+  end
+
+  def set_options_as_ivars options
     options.each do |name, value|
       instance_variable_set("@#{name}", value)
     end
@@ -11,6 +17,8 @@ class PrettyPrint
 
   def pp
     strip_whitespace
+    pp_blocks
+    pp_compact
     @doc.serialize(:save_with => 0)
   end
 
@@ -33,5 +41,41 @@ class PrettyPrint
     end
   end
 
+  def pp_blocks
+    @doc.css(@block.join(',')).each do |block|
+      unless block == @doc.root
+        block.add_previous_sibling "\n" if needs_hard_return? block
+        add_left_space block
+      end
+      add_internal_space block
+    end
+  end
+
+  def needs_hard_return? block
+    @compact.include? block.previous_element.andand.name or
+    block == block.parent.elements.first
+  end
+
+  def pp_compact
+    @doc.css(@compact.join(',')).each do |compact|
+      compact.add_previous_sibling "\n"
+      add_left_space compact
+    end
+  end
+
+  def add_left_space node
+    space_multiplier = node.ancestors.size - 1
+    space_multiplier.times do |x|
+      node.add_previous_sibling @tab
+    end
+  end
+
+  def add_internal_space node
+    space_multiplier = node.ancestors.size - 1
+    node.add_child "\n"
+    space_multiplier.times do |x|
+      node.add_child @tab
+    end
+  end
 
 end
--- test/test_prettyprint.rb
+++ test/test_prettyprint.rb
@@ -6,7 +6,7 @@ require_relative '../lib/prettyprint'
 class PrettyPrintTests < Minitest::Test
 
   OP1 = {
-    :block => %w(root),
+    :block => %w(root block structure div),
     :compact => %w(p),
     :inline => %w(i),
     :preserve_whitespace => true,
@@ -21,6 +21,13 @@ class PrettyPrintTests < Minitest::Test
     :tab => '  '
   }
 
+  OP3 = {
+    :block => %w(root),
+    :compact => %w(p),
+    :inline => %w(i),
+    :tab => '  '
+  }
+
   def setup_and_exercise options
     @parsed = Nokogiri.XML @input
     @pp = PrettyPrint.new(@parsed, options).pp
@@ -29,7 +36,7 @@ class PrettyPrintTests < Minitest::Test
   def test_strips_basic_whitespace_from_block_when_ws_is_true
     @input = "<root>  <p>stuff</p>  </root>"
     setup_and_exercise OP1
-    assert @pp !~ /  /
+    assert @pp.match /\n  <p>/
   end
 
   def test_leaves_inline_and_compact_space_when_ws_is_true
@@ -46,6 +53,48 @@ class PrettyPrintTests < Minitest::Test
     assert @pp =~ /<p\/>/
     assert @pp =~ /<i\/>/
     assert @pp !~ /<root>  <p>/
+  end
+
+  def test_ws_preserve_defaults_to_true_if_not_given
+    @input = "<root>  <p> </p><p>stuff<i> </i></p>  </root>"
+    setup_and_exercise OP3
+    assert @pp =~ /<p> <\/p>/
+    assert @pp =~ /<i> <\/i>/
+    assert @pp !~ /<root>  <p>/
+  end
+
+  def test_pp_blocks
+    @input = "<root>  <block><p> </p></block><p>stuff<i> </i></p>  </root>"
+    setup_and_exercise OP1
+    assert @pp.match /<root>\n  <block>\n    <p>/
+  end
+
+  def test_more_complex_example
+    @input = "<root>  <block>
+ <p> </p>
+    
+    </block><p>stuff<i> </i></p>  
+    
+    <structure>
+                       <div>
+                       <p>yo yo<i/></p>
+</div>
+</structure></root>"
+    setup_and_exercise OP1
+expected = '<?xml version="1.0"?>
+<root>
+  <block>
+    <p> </p>
+  </block>
+  <p>stuff<i> </i></p>
+  <structure>
+    <div>
+      <p>yo yo<i/></p>
+    </div>
+  </structure>
+</root>
+'
+assert @pp == expected
   end
 
 end
